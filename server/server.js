1d006d5f75679ba57cd478e3624bb7bc866d6ee8
YashvardhanJoshi1262 <yashjoshi1262@gmail.com>
2026-06-11 01:43:33 +0530

    added express server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "Expense Tracker API Running",
+  });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
