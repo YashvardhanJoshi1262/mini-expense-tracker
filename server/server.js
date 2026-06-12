e52d4905934082476c6f6925f3d6917ff75bd182
YashvardhanJoshi1262 <yashjoshi1262@gmail.com>
2026-06-13 00:04:16 +0530

    configured server port for deployment

--- server/server.js
+++ server/server.js
@@ -16,7 +16,8 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT =
+  process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
